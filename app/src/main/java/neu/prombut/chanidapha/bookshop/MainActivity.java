4d925843914e44da42e2c41879d8a8de3c5dede6
Chanidapha <peedee777888@gmail.com>
2016-06-21 15:15:42 +0700

    Welcome Finish

--- app/src/main/java/neu/prombut/chanidapha/bookshop/MainActivity.java
+++ app/src/main/java/neu/prombut/chanidapha/bookshop/MainActivity.java
@@ -1,10 +1,22 @@
 package neu.prombut.chanidapha.bookshop;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.speech.tts.Voice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     // Explicit
     private EditText userEditText, passwordEditText;
     private String userString, passwordString;
+    private static final String urlJSON = "http://swiftcodingthai.com/neu/get_user.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +36,83 @@ public class MainActivity extends AppCompatActivity {
         passwordEditText = (EditText) findViewById(R.id.editText5);
     } // Main Method
 
+    //Ceate Inner Class
+    private class MySynchronize extends AsyncTask<Void, Void, String> {
+
+        private Context context;
+        private String urlString;
+        private boolean statusABoolean = true;
+        private String truePasswordString;
+
+        public MySynchronize(String urlString, Context context) {
+            this.context = context;
+            this.urlString = urlString;
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(urlString).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+            }catch (Exception e){
+                return null;
+            }
+
+        } // doInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.d("BookShopV1", "JSON ==> " + s);
+
+            try {
+
+                JSONArray jsonArray = new JSONArray(s);
+                for (int i=0;i<jsonArray.length();i++) {
+
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                    if (userString.equals(jsonObject.getString("User"))){
+                        statusABoolean = false;
+                        truePasswordString = jsonObject.getString("Password");
+                    } // if
+
+                }  //for
+
+                //checkUser
+                if (statusABoolean){
+                    MyAlert myAlert = new MyAlert();
+                    myAlert.myDialog(context, "ไม่มี User นี้",
+                            "ไม่มี" + userString + "ในฐานข้อมูลของเรา");
+
+                }else  if (passwordString.equals(truePasswordString)){
+                    //Password True
+                    Toast.makeText(context, "Welcome User", Toast.LENGTH_SHORT).show();
+                }else {
+                    //Password False
+                    MyAlert myAlert = new MyAlert();
+                    myAlert.myDialog(context, "Password False");
+                            "Please Try Again Password False");
+
+                } // if
+
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+
+        } // onPost
+    } // Class
+
+
     public void clickSignIn(View view){
+
 
         //Get Value from Edit Text
         userString = userEditText.getText().toString().trim();
@@ -35,12 +124,21 @@ public class MainActivity extends AppCompatActivity {
             myAlert.myDialog(this, "Have Space", "Please Fill All Blank");
 
         }else {
-            //No Space
+
+            searchUserAnPassword();
+
 
         }
 
 
     } // ClickSignIn
+
+    private void searchUserAnPassword() {
+
+        MySynchronize mySynchronize = new MySynchronize(urlJSON, this);
+        mySynchronize.execute();
+
+    } //searchUser
 
     public void clickSignUpMain(View view) {
         startActivity(new Intent(MainActivity.this,SignUpActivity.class));
